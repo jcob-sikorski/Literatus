28be1a4d8268cf9d5c64c9b5d724f1525a8d5928
jcob-sikorski <45105669+jcob-sikorski@users.noreply.github.com>
2019-11-03 19:49:56 +0100

    Create rotate90Clockwise.py

---
+++ rotate90Clockwise.py
@@ -0,0 +1,13 @@
+# Function to rotate the matrix 
+# 90 degree clockwise 
+def rotate90Clockwise(A): 
+    N = len(A[0]) 
+    for i in range(N // 2): 
+        for j in range(i, N - i - 1): 
+            temp = A[i][j] 
+            A[i][j] = A[N - 1 - j][i] 
+            A[N - 1 - j][i] = A[N - 1 - i][N - 1 - j] 
+            A[N - 1 - i][N - 1 - j] = A[j][N - 1 - i] 
+            A[j][N - 1 - i] = temp
+
+    return A
